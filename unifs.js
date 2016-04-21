0f882a16ca3e02be84172e58f4b18e85b1006a0d
Vladimir Antonov <v.antonov@ndmsystems.com>
2016-04-21 13:45:57 +0300

    files

--- unifs.js
+++ unifs.js
@@ -1,14 +1,8 @@
-# nano-unifs
-A micro module for union of AJSFS/DirFS -> UniFS :)
-
-## Source
-
-```js
 var AJSFS = require('nano-ajsfs'),
     DirFS = require('nano-dirfs');
+
 
 module.exports = function newUniFS(root) {
 	return new (typeof root === 'string' ? DirFS : AJSFS)(root);
 };
 
-```
